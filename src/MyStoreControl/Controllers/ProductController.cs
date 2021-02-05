29d0dbae48684bc8e52375f0b91ccfcc93b6b797
Ricardo Tondello <rkdtondello@gmail.com>
2021-02-05 06:52:34 -0300

    Add Product API

---
+++ src/MyStoreControl/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Bogus;
+
+namespace MyStoreControl.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var id = 0;
+            var product = new Faker<Product>()
+                .RuleFor(o => o.Id, f => id++)
+                .RuleFor(o => o.Name, f => f.Commerce.ProductName())
+                .RuleFor(o => o.Stock, f => f.Random.Int(1, 100))
+                .RuleFor(o => o.Price, f => f.Random.Double(1, 1000));
+    
+            return product.Generate(new Random().Next(1, 1_100));
+        }
+    }
+}
---
+++ src/MyStoreControl/Product.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyStoreControl
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Stock {get; set; }
+    }
+}
