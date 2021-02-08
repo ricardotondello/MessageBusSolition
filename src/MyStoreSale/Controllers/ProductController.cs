40d5bba2472caef8c58f87d21b965f93dcbb5c78
Ricardo Tondello <rkdtondello@gmail.com>
2021-02-08 06:54:08 -0300

    feat: Add sales project

---
+++ src/MyStoreSale/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MyStoreSale.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
