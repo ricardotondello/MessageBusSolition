0db11172c59c6521fc4ffff328256c372dccc108
Ricardo Tondello <rkdtondello@gmail.com>
2021-02-08 10:57:42 -0300

    feat: Caching data from REDIS

--- src/MyStoreSale/Service/Store.cs
+++ src/MyStoreSale/Service/Store.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Configuration;
+using MyStoreSale.Extensions;
 using MyStoreSale.Models;
 using MyStoreSale.Service.Interface;
 using Newtonsoft.Json;
@@ -12,12 +15,25 @@ namespace MyStoreSale.Service
     {
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly IConfiguration _configuration;
-        public Store(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        private static IDistributedCache _cache;
+        private static string recordKey => nameof(Product) + "_" + DateTime.Now.ToString("yyyyMMdd_hhmm");
+        public Store(IHttpClientFactory httpClientFactory, IConfiguration configuration, IDistributedCache cache)
         {
             _httpClientFactory = httpClientFactory;
             _configuration = configuration;
+            _cache = cache;
         }
         public async Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            var list =  await _cache.GetRecordAsync<IEnumerable<Product>>(recordKey);
+            if (list != null) return list;
+
+            list = await GetProductsHttpClient();
+            await _cache.SetRecordAsync(recordKey, list);
+            return list;
+        }
+
+        private async Task<IEnumerable<Product>> GetProductsHttpClient()
         {
             var resp = await _httpClientFactory.CreateClient().GetAsync(GetURL());
             resp.EnsureSuccessStatusCode();
@@ -26,7 +42,6 @@ namespace MyStoreSale.Service
 
             return await Task.FromResult(list);
         }
-
         private string GetURL() => _configuration["URL_Products"];
     }
 }
--- src/MyStoreSale/Startup.cs
+++ src/MyStoreSale/Startup.cs
@@ -25,6 +25,12 @@ namespace MyStoreSale
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddStackExchangeRedisCache(options => 
+            {
+                options.Configuration = Configuration.GetConnectionString("Redis");
+                options.InstanceName = nameof(MyStoreSale);
+            });
+
             services.AddControllersWithViews();
             services.AddHttpClient();
             services.AddScoped<IStore, Store>();
