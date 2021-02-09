3b0949d92a88eb7d12a0fe10e0fa897332901fc5
Ricardo Tondello <rkdtondello@gmail.com>
2021-02-09 16:07:25 -0300

    Add Integration Tests

---
+++ test/MyStoreControl.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace MyStoreControl.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        //services.RemoveAll(typeof(DataContext));
+
+                    });
+                });
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", await GetJWTAsync());
+        }
+
+        private async Task<string> GetJWTAsync()
+        {
+            return "your JWT";
+        }
+    }
+}
---
+++ test/MyStoreControl.IntegrationTests/ProductIntegrationTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using SharedKernel;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace MyStoreControl.IntegrationTests
+{
+    public class ProductIntegrationTest : IntegrationTest
+    {
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public ProductIntegrationTest(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Fact]
+        public async Task Get_Returns_success()
+        {
+            //Arrange
+            
+            //Act
+            var response = TestClient.GetAsync("/Product");
+            
+            //Assert
+
+            response.Result.StatusCode.Should().Be(StatusCodes.Status200OK);
+
+            var items = ((await response.Result.Content.ReadFromJsonAsync<IEnumerable<Product>>()) ?? Array.Empty<Product>()).ToList();
+            
+            items.Should().NotBeNull();
+            items.Should().HaveCountGreaterThan(0);
+            
+        }
+    }
+}
